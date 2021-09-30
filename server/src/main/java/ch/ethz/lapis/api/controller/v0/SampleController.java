43282c3c6debcd8086fc86702b694c358dd9a6f5
Chaoran Chen <mail@chaoran-chen.de>
2021-09-30 21:33:01 +0200

    Transform responses explicitly to JSON strings

--- server/src/main/java/ch/ethz/lapis/api/controller/v0/SampleController.java
+++ server/src/main/java/ch/ethz/lapis/api/controller/v0/SampleController.java
@@ -5,6 +5,8 @@ import ch.ethz.lapis.api.entity.SequenceType;
 import ch.ethz.lapis.api.entity.req.SampleAggregatedRequest;
 import ch.ethz.lapis.api.entity.req.SampleDetailRequest;
 import ch.ethz.lapis.api.entity.res.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,36 +19,54 @@ import java.util.List;
 public class SampleController {
 
     private final SampleService sampleService;
+    private final ObjectMapper objectMapper;
 
-    public SampleController(SampleService sampleService) {
+    public SampleController(SampleService sampleService, ObjectMapper objectMapper) {
         this.sampleService = sampleService;
+        this.objectMapper = objectMapper;
     }
 
-    @GetMapping("/aggregated")
-    public V0Response<SampleAggregatedResponse> getAggregated(SampleAggregatedRequest request) throws SQLException {
+    @GetMapping(
+            value = "/aggregated",
+            produces = "application/json"
+    )
+    public String getAggregated(SampleAggregatedRequest request) throws SQLException, JsonProcessingException {
         List<SampleAggregated> aggregatedSamples = sampleService.getAggregatedSamples(request);
-        return new V0Response<>(new SampleAggregatedResponse(
+        V0Response<SampleAggregatedResponse> response = new V0Response<>(new SampleAggregatedResponse(
                 request.getFields(),
                 aggregatedSamples
         ));
+        return objectMapper.writeValueAsString(response);
     }
 
-    @GetMapping("/details")
-    public V0Response<SampleDetailResponse> getDetails(SampleDetailRequest request) throws SQLException {
+    @GetMapping(
+            value = "/details",
+            produces = "application/json"
+    )
+    public String getDetails(SampleDetailRequest request) throws SQLException, JsonProcessingException {
         List<SampleDetail> samples = sampleService.getDetailedSamples(request);
-        return new V0Response<>(new SampleDetailResponse(samples));
+        V0Response<SampleDetailResponse> response = new V0Response<>(new SampleDetailResponse(samples));
+        return objectMapper.writeValueAsString(response);
     }
 
-    @GetMapping("/aa-mutations")
-    public V0Response<SampleMutationsResponse> getAAMutations(SampleDetailRequest request) throws SQLException {
+    @GetMapping(
+            value = "/aa-mutations",
+            produces = "application/json"
+    )
+    public String getAAMutations(SampleDetailRequest request) throws SQLException, JsonProcessingException {
         SampleMutationsResponse mutationsResponse = sampleService.getMutations(request, SequenceType.AMINO_ACID);
-        return new V0Response<>(mutationsResponse);
+        V0Response<SampleMutationsResponse> response = new V0Response<>(mutationsResponse);
+        return objectMapper.writeValueAsString(response);
     }
 
-    @GetMapping("/nuc-mutations")
-    public V0Response<SampleMutationsResponse> getNucMutations(SampleDetailRequest request) throws SQLException {
+    @GetMapping(
+            value = "/nuc-mutations",
+            produces = "application/json"
+    )
+    public String getNucMutations(SampleDetailRequest request) throws SQLException, JsonProcessingException {
         SampleMutationsResponse mutationsResponse = sampleService.getMutations(request, SequenceType.NUCLEOTIDE);
-        return new V0Response<>(mutationsResponse);
+        V0Response<SampleMutationsResponse> response = new V0Response<>(mutationsResponse);
+        return objectMapper.writeValueAsString(response);
     }
 
     @GetMapping(
