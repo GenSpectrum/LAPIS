93e39a978bb748b3022e0c4001051d4c98fac71d
Chaoran Chen <mail@chaoran-chen.de>
2022-06-24 15:35:15 +0200

    feat: load some metadata (strings and dates) into the database

--- server_v2/src/main.rs
+++ server_v2/src/main.rs
@@ -6,6 +6,7 @@ use crate::base::db;
 use serde::Deserialize;
 use chrono::Local;
 use std::{thread, time};
+use std::path::Path;
 use crate::base::ProgramConfig;
 
 
@@ -19,18 +20,19 @@ fn main() {
     let config: ProgramConfig = config_unparsed.try_deserialize()
         .expect("The config file is invalid.");
 
-    db::generate_db_tables(&config.database, &config.schema);
+    // db::generate_db_tables(&config.database, &config.schema);
+    base::proc::load_source_data(Path::new("/Users/chachen/polybox/tmp_mpox/data"), &config.schema, &config.database);
 
-    let db = Database::load(&config.database);
-
-    for _ in 0..20 {
-        let ids = random_ids(db.size, 0.2);
-        let start = time::Instant::now();
-        let nucs = db.nuc_muts(&ids);
-        let duration = start.elapsed();
-        println!("In {} sequences, I found a total of {} nucleotide mutations ({}ms)", ids.len(), nucs.len(), duration.as_millis());
-        thread::sleep(time::Duration::from_secs(3));
-    }
+    // let db = Database::load(&config.database);
+    //
+    // for _ in 0..20 {
+    //     let ids = random_ids(db.size, 0.2);
+    //     let start = time::Instant::now();
+    //     let nucs = db.nuc_muts(&ids);
+    //     let duration = start.elapsed();
+    //     println!("In {} sequences, I found a total of {} nucleotide mutations ({}ms)", ids.len(), nucs.len(), duration.as_millis());
+    //     thread::sleep(time::Duration::from_secs(3));
+    // }
 
 }
 
