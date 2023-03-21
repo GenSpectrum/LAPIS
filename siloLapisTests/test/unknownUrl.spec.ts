8c17409b3f573c9e7b1fb96996f979f4c11e372a
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-03-21 16:49:34 +0100

    test: test 404 page in end to end test
    #184

---
+++ siloLapisTests/test/unknownUrl.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { basePath } from './common';
+
+describe('SILO-LAPIS', () => {
+  it('should return a 404 JSON response by default', async () => {
+    const result = await fetch(basePath + '/unknownUrl');
+
+    expect(result.status).equals(404);
+    expect(result.headers.get('Content-Type')).equals('application/json');
+    expect((await result.json())?.error).equals('Not Found');
+  });
+
+  it('should return a 404 HTML response when a browser asks for HTML', async () => {
+    const result = await fetch(basePath + '/unknownUrl', { headers: { Accept: 'text/html' } });
+
+    expect(result.status).equals(404);
+    expect(result.headers.get('Content-Type')).equals('text/html');
+
+    let responseBody = await result.text();
+    expect(responseBody).contains('Page not found');
+    expect(responseBody).contains('a href="/swagger-ui/index.html"');
+  });
+});
