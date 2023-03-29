a31de79651c6242c17d6e97d457252b59bcd07b3
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-03-29 16:38:49 +0200

    feat: POC: show which fields are available in the request in Swagger UI from a config
    issue: #179

--- lapis2/src/main/kotlin/org/genspectrum/lapis/controller/LapisController.kt
+++ lapis2/src/main/kotlin/org/genspectrum/lapis/controller/LapisController.kt
@@ -1,16 +1,22 @@
 package org.genspectrum.lapis.controller
 
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
 import org.genspectrum.lapis.model.AggregatedModel
 import org.genspectrum.lapis.response.AggregatedResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+const val REQUEST_SCHEMA = "SequenceFilters"
+
 @RestController
 class LapisController(val aggregatedModel: AggregatedModel) {
     @GetMapping("/aggregated")
     fun aggregated(
-        @RequestParam filterParameter: Map<String, String>,
+        @Parameter(schema = Schema(ref = "#/components/schemas/$REQUEST_SCHEMA"))
+        @RequestParam
+        filterParameter: Map<String, String>,
     ): AggregatedResponse {
         return aggregatedModel.handleRequest(filterParameter)
     }
