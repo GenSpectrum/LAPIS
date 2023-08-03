27ecd70116a9526954c1b83c1091cfba9d11261b
Jonas Kellerer <jonas.kellerer@tngtech.com>
2023-08-03 12:01:38 +0200

    feat: return data version in each header

--- lapis2/src/main/kotlin/org/genspectrum/lapis/silo/SiloClient.kt
+++ lapis2/src/main/kotlin/org/genspectrum/lapis/silo/SiloClient.kt
@@ -2,6 +2,7 @@ package org.genspectrum.lapis.silo
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KotlinLogging
+import org.genspectrum.lapis.request.DataVersion
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -9,14 +10,20 @@ import org.springframework.stereotype.Component
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.net.http.HttpResponse.BodyHandlers
 
 private val log = KotlinLogging.logger {}
 
 @Component
-class SiloClient(@Value("\${silo.url}") private val siloUrl: String, private val objectMapper: ObjectMapper) {
-
-    fun <ResponseType> sendQuery(query: SiloQuery<ResponseType>): ResponseType {
+class SiloClient(
+    @Value("\${silo.url}") private val siloUrl: String,
+    private val objectMapper: ObjectMapper,
+    private val dataVersion: DataVersion,
+) {
+    fun <ResponseType> sendQuery(
+        query: SiloQuery<ResponseType>,
+    ): ResponseType {
         val client = HttpClient.newHttpClient()
         val request = HttpRequest.newBuilder(URI("$siloUrl/query"))
             .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -39,12 +46,18 @@ class SiloClient(@Value("\${silo.url}") private val siloUrl: String, private val
             throw SiloException(response.body(), null)
         }
 
+        addDataVersion(response)
+
         try {
             return objectMapper.readValue(response.body(), query.action.typeReference).queryResult
         } catch (exception: Exception) {
             val message = "Could not parse response from silo: " + exception::class.toString() + " " + exception.message
             throw SiloException(message, exception)
         }
+    }
+
+    private fun addDataVersion(response: HttpResponse<String>) {
+        dataVersion.dataVersion = response.headers().firstValue("data-version").orElse("")
     }
 }
 
