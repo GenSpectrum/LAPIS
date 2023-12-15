b703a9c2ebe569213507ca72726c325caf944aa2
Jonas Kellerer <jonas.kellerer@tngtech.com>
2023-12-15 10:16:05 +0100

    feat: add sample to info route

--- lapis2/src/main/kotlin/org/genspectrum/lapis/controller/InfoController.kt
+++ lapis2/src/main/kotlin/org/genspectrum/lapis/controller/InfoController.kt
@@ -5,11 +5,13 @@ import org.genspectrum.lapis.model.SiloQueryModel
 import org.genspectrum.lapis.request.LapisInfo
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 const val INFO_ROUTE = "/info"
 
 @RestController
+@RequestMapping("/sample")
 class InfoController(private val siloQueryModel: SiloQueryModel) {
     @GetMapping(INFO_ROUTE, produces = [MediaType.APPLICATION_JSON_VALUE])
     @Operation(description = INFO_ENDPOINT_DESCRIPTION)
