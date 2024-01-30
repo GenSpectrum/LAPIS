d2658098bb8178d92b42b4b79cf3768be7d9b259
Jonas Kellerer <jonas.kellerer@tngtech.com>
2024-01-30 09:27:11 +0100

    fix: end to end tests failing due to timeout and new pango lineage alias in return

--- lapis2-docs/playwright.config.ts
+++ lapis2-docs/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: 1,
     workers: process.env.CI ? 1 : undefined,
-    timeout: 60000,
+    timeout: 90000,
     reporter: 'html',
     use: {
         trace: 'retain-on-failure',
