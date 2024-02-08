340f2acf08ae49d6f9fb1965f1971fd22e21f618
Jonas Kellerer <jonas.kellerer@tngtech.com>
2024-02-08 15:56:13 +0100

    feat: add access key parameter to /sample/info and remove downloadAsFile

--- lapis2/src/main/kotlin/org/genspectrum/lapis/openApi/AccessKeyParameterCustomizer.kt
+++ lapis2/src/main/kotlin/org/genspectrum/lapis/openApi/AccessKeyParameterCustomizer.kt
@@ -2,31 +2,39 @@ package org.genspectrum.lapis.openApi
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.parameters.Parameter
-import org.genspectrum.lapis.controller.DOWNLOAD_AS_FILE_PROPERTY
+import org.genspectrum.lapis.config.DatabaseConfig
+import org.genspectrum.lapis.config.OpennessLevel
+import org.genspectrum.lapis.controller.ACCESS_KEY_PROPERTY
 import org.genspectrum.lapis.controller.INFO_ROUTE
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.stereotype.Component
 
 @Component
-class DownloadAsFileParameterCustomizer : OpenApiCustomizer {
+class AccessKeyParameterCustomizer(
+    private val databaseConfig: DatabaseConfig,
+) : OpenApiCustomizer {
     companion object {
-        private val PATH_WITHOUT_DOWNLOAD_AS_FILE = listOf(
+        private val PATH_WITH_ACCESS_KEY_PARAMETER = listOf(
             "/sample$INFO_ROUTE",
         )
     }
 
     override fun customise(openApi: OpenAPI) {
+        if (databaseConfig.schema.opennessLevel == OpennessLevel.OPEN) {
+            return
+        }
+
         for ((_, path) in openApi.paths.filter { (url, _) ->
-            url.startsWith("/sample") && !PATH_WITHOUT_DOWNLOAD_AS_FILE.any {
+            PATH_WITH_ACCESS_KEY_PARAMETER.any {
                 url.startsWith(it)
             }
         }) {
             path.get.addParametersItem(
                 Parameter()
                     .`in`("query")
-                    .name(DOWNLOAD_AS_FILE_PROPERTY)
-                    .description(DOWNLOAD_AS_FILE_DESCRIPTION)
-                    .schema(downloadAsFileSchema()),
+                    .name(ACCESS_KEY_PROPERTY)
+                    .description(ACCESS_KEY_DESCRIPTION)
+                    .schema(accessKeySchema()),
             )
         }
     }
