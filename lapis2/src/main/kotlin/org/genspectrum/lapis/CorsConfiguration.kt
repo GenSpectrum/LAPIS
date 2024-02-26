4877c04054b274d8db9c7cc73f337cc8120126b9
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-02-26 16:46:31 +0100

    feat: make headers accessible to browser despite CORS

--- lapis2/src/main/kotlin/org/genspectrum/lapis/CorsConfiguration.kt
+++ lapis2/src/main/kotlin/org/genspectrum/lapis/CorsConfiguration.kt
@@ -1,6 +1,9 @@
 package org.genspectrum.lapis
 
+import org.genspectrum.lapis.openApi.REQUEST_ID_HEADER
+import org.genspectrum.lapis.request.LAPIS_DATA_VERSION_HEADER
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders.RETRY_AFTER
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -11,6 +14,7 @@ class CorsConfiguration : WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "OPTIONS")
             .allowedHeaders("*")
+            .exposedHeaders(LAPIS_DATA_VERSION_HEADER, REQUEST_ID_HEADER, RETRY_AFTER)
             .maxAge(3600)
     }
 }
