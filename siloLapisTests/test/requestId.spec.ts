eb7539164962f814d60e4920818d3f13c76c67e1
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-05-15 15:41:54 +0200

    fix(lapis2): bring back request id header in OpenAPI docs
    resolves #627

--- siloLapisTests/test/requestId.spec.ts
+++ siloLapisTests/test/requestId.spec.ts
@@ -1,28 +1,23 @@
 import { expect } from 'chai';
-import { basePath } from './common';
+import { lapisClient } from './common';
 
 describe('The request id', () => {
   it('should be returned when explicitly specified', async () => {
     const requestID = 'hardcodedRequestIdInTheTest';
 
-    // TODO(#627): bring back the old tests using the generated client
-    // const result = await lapisClient.postAggregated1({
-    //   aggregatedPostRequest: {},
-    //   xRequestID: requestID,
-    // });
-    const result = await fetch(basePath + '/sample/aggregated', { headers: { 'X-Request-Id': requestID } });
+    const result = await lapisClient.postAggregated1({
+      aggregatedPostRequest: {},
+      xRequestID: requestID,
+    });
 
-    // expect(result.info.requestId).equals(requestID);
-    expect((await result.json()).info.requestId).equals(requestID);
+    expect(result.info.requestId).equals(requestID);
   });
 
   it('should be generated when none is specified', async () => {
-    // const result = await lapisClient.postAggregated1({
-    //   aggregatedPostRequest: {},
-    // });
-    const result = await fetch(basePath + '/sample/aggregated');
+    const result = await lapisClient.postAggregated1({
+      aggregatedPostRequest: {},
+    });
 
-    // expect(result.info.requestId).length.is.at.least(1);
-    expect((await result.json()).info.requestId).length.is.at.least(1);
+    expect(result.info.requestId).length.is.at.least(1);
   });
 });
