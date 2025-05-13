fe2dd2c7dd38e33e381d82d60bd031bb7f014f80
Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
2025-05-13 08:46:07 +0200

    refactor(lapis): it's cleaner to hand out `null` when getting an unknown sequence from the reference genome (#1179)
    We don't know yet whether we want to handle that as a "bad request" at this point.

--- lapis/src/main/kotlin/org/genspectrum/lapis/model/VariantQueryCustomListener.kt
+++ lapis/src/main/kotlin/org/genspectrum/lapis/model/VariantQueryCustomListener.kt
@@ -19,6 +19,7 @@ import mu.KotlinLogging
 import org.antlr.v4.runtime.RuleContext
 import org.antlr.v4.runtime.tree.ParseTreeListener
 import org.genspectrum.lapis.config.ReferenceGenomeSchema
+import org.genspectrum.lapis.controller.BadRequestException
 import org.genspectrum.lapis.request.ESCAPED_STOP_CODON
 import org.genspectrum.lapis.request.LAPIS_INSERTION_AMBIGUITY_SYMBOL
 import org.genspectrum.lapis.request.SILO_INSERTION_AMBIGUITY_SYMBOL
@@ -134,7 +135,9 @@ class VariantQueryCustomListener(
 
     override fun enterAaMutationQuery(ctx: AaMutationQueryContext) {
         val position = ctx.position().text.toInt()
-        val gene = referenceGenomeSchema.getGene(ctx.gene().text).name
+        val geneName = ctx.gene().text
+        val gene = referenceGenomeSchema.getGene(geneName)?.name
+            ?: throw BadRequestException("Unknown gene: $geneName")
 
         val expression = when (val aaSymbol = ctx.possiblyAmbiguousAaSymbol()) {
             null -> HasAminoAcidMutation(gene, position)
@@ -146,7 +149,9 @@ class VariantQueryCustomListener(
 
     override fun enterAaInsertionQuery(ctx: AaInsertionQueryContext) {
         val value = ctx.aaInsertionSymbol().joinToString("", transform = ::mapInsertionSymbol)
-        val gene = referenceGenomeSchema.getGene(ctx.gene().text).name
+        val geneName = ctx.gene().text
+        val gene = referenceGenomeSchema.getGene(geneName)?.name
+            ?: throw BadRequestException("Unknown gene: $geneName")
 
         expressionStack.addLast(
             AminoAcidInsertionContains(
