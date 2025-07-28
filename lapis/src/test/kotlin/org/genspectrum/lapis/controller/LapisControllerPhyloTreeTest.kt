b2f5358224ad060d2b460e422874dd50c192f929
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-07-28 12:24:45 +0200

    feat(lapis): add post tests

--- lapis/src/test/kotlin/org/genspectrum/lapis/controller/LapisControllerPhyloTreeTest.kt
+++ lapis/src/test/kotlin/org/genspectrum/lapis/controller/LapisControllerPhyloTreeTest.kt
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
@@ -42,6 +43,20 @@ class LapisControllerPhyloTreeTest(
             .andExpect(status().isBadRequest)
             .andExpect(header().string("Content-Type", "application/problem+json"))
             .andExpect(jsonPath("\$.detail", startsWith("Required parameter 'phyloTreeField' is not present")))
+
+        mockMvc.perform(
+            postSample("/mostRecentCommonAncestor")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""{}"""),
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(header().string("Content-Type", "application/json"))
+            .andExpect(
+                jsonPath(
+                    "\$.error.detail",
+                    startsWith("phyloTreeField is required and must be a string representing a phylo tree field"),
+                ),
+            )
     }
 
     @Test
@@ -65,14 +80,24 @@ class LapisControllerPhyloTreeTest(
     }
 
     @Test
-    fun `GIVEN call to mostRecentCommonAncestor endpoint with invalid PhyloTreeField returns error`() {
-        mockMvc.perform(getSample("/mostRecentCommonAncestor?phyloTreeField=floatValue"))
-            .andExpect(status().isBadRequest)
-            .andExpect(
-                jsonPath(
-                    "$.error.detail",
-                    containsString("'floatValue' is not a phylo tree field, known phylo tree fields are [primaryKey]"),
-                ),
-            )
+    fun `GIVEN call to mostRecentCommonAncestor endpoint with invalid PhyloTreeField returns 400 error`() {
+        listOf(
+            mockMvc.perform(getSample("/mostRecentCommonAncestor?phyloTreeField=floatValue")),
+            mockMvc.perform(
+                postSample("/mostRecentCommonAncestor")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""{"phyloTreeField": "floatValue"}"""),
+            ),
+        ).forEach {
+            it.andExpect(status().isBadRequest)
+                .andExpect(
+                    jsonPath(
+                        "$.error.detail",
+                        containsString(
+                            "'floatValue' is not a phylo tree field, known phylo tree fields are [primaryKey]",
+                        ),
+                    ),
+                )
+        }
     }
 }
