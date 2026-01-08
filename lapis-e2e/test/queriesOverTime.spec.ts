2e0dfa0f005424e9d480de03ff2c387d71fb06f1
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2026-01-08 10:21:26 +0100

    1480 e2e test

--- lapis-e2e/test/queriesOverTime.spec.ts
+++ lapis-e2e/test/queriesOverTime.spec.ts
@@ -2,6 +2,48 @@ import { expect } from 'chai';
 import { queriesOverTimeClient } from './common';
 
 describe('The /mutationsOverTime endpoint', () => {
+  it('should return a correct queriesOverTime response', async () => {
+    const result = await queriesOverTimeClient.postQueriesOverTime({
+      queriesOverTimeRequest: {
+        filters: {
+          country: 'Switzerland',
+        },
+        queries: [
+          { countQuery: 'C27972T | C26735T', coverageQuery: '!27972N & !26735N', displayLabel: 'my label' },
+          { countQuery: 'C27972T & C26735T', coverageQuery: '!27972N & !26735N' },
+        ],
+        dateRanges: [
+          { dateFrom: '2020-01-01', dateTo: '2020-12-31' },
+          { dateFrom: '2021-01-01', dateTo: '2021-12-31' },
+          { dateFrom: '2022-01-01', dateTo: '2022-12-31' },
+        ],
+        dateField: 'date',
+      },
+    });
+
+    expect(result.data).to.deep.equal({
+      queries: ['my label', 'C27972T & C26735T'],
+      dateRanges: [
+        { dateFrom: new Date('2020-01-01'), dateTo: new Date('2020-12-31') },
+        { dateFrom: new Date('2021-01-01'), dateTo: new Date('2021-12-31') },
+        { dateFrom: new Date('2022-01-01'), dateTo: new Date('2022-12-31') },
+      ],
+      data: [
+        [
+          { count: 6, coverage: 22 },
+          { count: 52, coverage: 73 },
+          { count: 0, coverage: 0 },
+        ],
+        [
+          { count: 0, coverage: 22 },
+          { count: 1, coverage: 73 },
+          { count: 0, coverage: 0 },
+        ],
+      ],
+      totalCountsByDateRange: [22, 77, 0],
+    });
+  });
+
   it('returns a response with the correct dimensions etc.', async () => {
     const result = await queriesOverTimeClient.postNucleotideMutationsOverTime({
       mutationsOverTimeRequest: {
