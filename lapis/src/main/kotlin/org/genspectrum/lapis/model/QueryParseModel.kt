a3958c0a9549bf0c4f50ed26a3833a2f1a9bfdb0
Felix Hennig <mail@felixhennig.com>
2026-02-12 10:23:43 +0100

    Fetch the dataVersion

--- lapis/src/main/kotlin/org/genspectrum/lapis/model/QueryParseModel.kt
+++ lapis/src/main/kotlin/org/genspectrum/lapis/model/QueryParseModel.kt
@@ -2,14 +2,21 @@ package org.genspectrum.lapis.model
 
 import org.genspectrum.lapis.controller.BadRequestException
 import org.genspectrum.lapis.response.ParsedQueryResult
+import org.genspectrum.lapis.silo.SiloClient
 import org.springframework.stereotype.Component
 
 @Component
 class QueryParseModel(
+    private val siloClient: SiloClient,
     private val advancedQueryFacade: AdvancedQueryFacade,
 ) {
-    fun parseQueries(queries: List<String>): List<ParsedQueryResult> =
-        queries.map { query ->
+    fun parseQueries(queries: List<String>): List<ParsedQueryResult> {
+        try {
+            siloClient.callInfo() // populates dataVersion.dataVersion
+        } catch (e: Exception) {
+            // If callInfo fails, continue with null dataVersion
+        }
+        return queries.map { query ->
             try {
                 val filter = advancedQueryFacade.map(query)
                 ParsedQueryResult(filter = filter, error = null)
@@ -22,4 +29,5 @@ class QueryParseModel(
                 )
             }
         }
+    }
 }
