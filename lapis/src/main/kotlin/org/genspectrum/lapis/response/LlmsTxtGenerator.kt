f2ee5486aea1cfc28e3c1663505d75e890f236ba
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2026-02-20 09:54:23 +0100

    1544 wip

--- lapis/src/main/kotlin/org/genspectrum/lapis/response/LlmsTxtGenerator.kt
+++ lapis/src/main/kotlin/org/genspectrum/lapis/response/LlmsTxtGenerator.kt
@@ -3,6 +3,18 @@ package org.genspectrum.lapis.response
 import org.genspectrum.lapis.config.DatabaseConfig
 import org.genspectrum.lapis.config.MetadataType
 import org.genspectrum.lapis.config.ReferenceGenomeSchema
+import org.genspectrum.lapis.controller.SampleRoute
+import org.genspectrum.lapis.controller.SampleRoute.AGGREGATED
+import org.genspectrum.lapis.controller.SampleRoute.ALIGNED_AMINO_ACID_SEQUENCES
+import org.genspectrum.lapis.controller.SampleRoute.ALIGNED_NUCLEOTIDE_SEQUENCES
+import org.genspectrum.lapis.controller.SampleRoute.AMINO_ACID_INSERTIONS
+import org.genspectrum.lapis.controller.SampleRoute.AMINO_ACID_MUTATIONS
+import org.genspectrum.lapis.controller.SampleRoute.DETAILS
+import org.genspectrum.lapis.controller.SampleRoute.MOST_RECENT_COMMON_ANCESTOR
+import org.genspectrum.lapis.controller.SampleRoute.NUCLEOTIDE_INSERTIONS
+import org.genspectrum.lapis.controller.SampleRoute.NUCLEOTIDE_MUTATIONS
+import org.genspectrum.lapis.controller.SampleRoute.PHYLO_SUBTREE
+import org.genspectrum.lapis.controller.SampleRoute.UNALIGNED_NUCLEOTIDE_SEQUENCES
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -17,6 +29,7 @@ class LlmsTxtGenerator(
 
         sections.add(generateHeader())
         sections.add(generateInstanceConfigurationSection())
+        sections.add(generateApiEndpointsSection())
         // TODO: Add more sections
 
         return sections.joinToString("\n\n")
@@ -86,6 +99,55 @@ Available genes for amino acid queries:
 Genes: $geneNames
             """.trimIndent()
     }
+
+    private fun generateApiEndpointsSection(): String {
+        val hasPhyloTreeField = databaseConfig.schema.metadata.any { it.isPhyloTreeField }
+
+        val phylogeneticSection = if (hasPhyloTreeField) {
+            """
+            
+### Phylogenetic Analysis
+
+- [${getSampleUrl(MOST_RECENT_COMMON_ANCESTOR)}](${getSampleUrl(MOST_RECENT_COMMON_ANCESTOR)}): Find most recent common ancestor for queried sequences
+- [${getSampleUrl(PHYLO_SUBTREE)}](${getSampleUrl(PHYLO_SUBTREE)}): Get phylogenetic subtree in Newick format
+            """.trimIndent()
+        } else {
+            ""
+        }
+
+        return """
+## API Endpoints
+
+### Data Retrieval
+
+- [${getSampleUrl(AGGREGATED)}](${getSampleUrl(AGGREGATED)}): Count and group sequences by metadata and mutations
+- [${getSampleUrl(DETAILS)}](${getSampleUrl(DETAILS)}): Retrieve detailed metadata for matching sequences
+- [${getSampleUrl(ALIGNED_NUCLEOTIDE_SEQUENCES)}](${getSampleUrl(ALIGNED_NUCLEOTIDE_SEQUENCES)}): Get aligned nucleotide sequences in FASTA format
+- [${getSampleUrl(UNALIGNED_NUCLEOTIDE_SEQUENCES)}](${getSampleUrl(UNALIGNED_NUCLEOTIDE_SEQUENCES)}): Get unaligned nucleotide sequences
+- [${getSampleUrl(ALIGNED_AMINO_ACID_SEQUENCES)}/{gene}](${getSampleUrl(ALIGNED_AMINO_ACID_SEQUENCES)}): Get aligned amino acid sequences for a specific gene
+
+### Mutation Analysis
+
+- [${getSampleUrl(NUCLEOTIDE_MUTATIONS)}](${getSampleUrl(NUCLEOTIDE_MUTATIONS)}): List nucleotide mutations with their proportions
+- [${getSampleUrl(AMINO_ACID_MUTATIONS)}](${getSampleUrl(AMINO_ACID_MUTATIONS)}): List amino acid mutations with their proportions
+- [${getSampleUrl(NUCLEOTIDE_INSERTIONS)}](${getSampleUrl(NUCLEOTIDE_INSERTIONS)}): List nucleotide insertions
+- [${getSampleUrl(AMINO_ACID_INSERTIONS)}](${getSampleUrl(AMINO_ACID_INSERTIONS)}): List amino acid insertions
+
+### Time Series
+
+- [/sample/queriesOverTime](${getDocsUrl("concepts/queries-over-time")}): Query results aggregated over time
+$phylogeneticSection
+
+### Utility
+
+- [/sample/info](${getDocsUrl("references/introduction")}): Get instance information and versions
+- [/sample/getDatabaseConfig](${getDocsUrl("references/database-configuration")}): Retrieve the complete database configuration
+
+All endpoints support both GET and POST methods. POST requests accept JSON or form-encoded data.
+            """.trimIndent()
+    }
+
+    private fun getSampleUrl(route: SampleRoute): String = "sample${route.pathSegment}"
 
     private fun getDocsUrl(path: String): String =
         if (lapisDocsUrl.isNotBlank()) {
